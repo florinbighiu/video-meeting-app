59488f72e7e5180e5cbeac4744f6b5f4108a4071
florinbighiu <120215264+florinbighiu@users.noreply.github.com>
2024-04-19 18:10:26 +0300

    initial commit

---
+++ server/src/test/java/com/videomeetingapp/VideoMeetingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.videomeetingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoMeetingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
